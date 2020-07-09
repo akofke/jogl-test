9a2f26e17df2af05fd23f9c5690b8b0f38e2c186
Alex Kofke <akofke@gmail.com>
2020-07-09 18:15:49 -0400

    Instances

--- src/main/java/Main2.java
+++ src/main/java/Main2.java
@@ -1,4 +1,6 @@
 import com.jogamp.common.net.Uri;
+import com.jogamp.nativewindow.NativeWindow;
+import com.jogamp.nativewindow.ScalableSurface;
 import com.jogamp.opengl.*;
 import com.jogamp.opengl.awt.GLCanvas;
 import com.jogamp.opengl.awt.GLJPanel;
@@ -7,19 +9,27 @@ import com.jogamp.opengl.util.*;
 import com.jogamp.opengl.util.glsl.ShaderCode;
 import com.jogamp.opengl.util.glsl.ShaderProgram;
 import com.jogamp.opengl.util.glsl.ShaderState;
+import jogamp.nativewindow.SurfaceScaleUtils;
+import jogamp.nativewindow.macosx.OSXUtil;
 import org.joml.Matrix4f;
 import org.joml.Vector3f;
+import org.joml.Vector4f;
 import org.joml.camera.ArcBallCamera;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionListener;
+import java.awt.event.MouseWheelEvent;
+import java.awt.event.MouseWheelListener;
 import java.nio.ByteBuffer;
 import java.nio.FloatBuffer;
 import java.nio.IntBuffer;
 import java.time.Duration;
 import java.time.Instant;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.SplittableRandom;
 
 public class Main2 {
     public static void main(String[] args) {
@@ -34,9 +44,12 @@ public class Main2 {
 
             var panel = new GLCanvas(caps);
 //            var panel = new GLJPanel(caps);
+//            panel.setSkipGLOrientationVerticalFlip(true);
             panel.setPreferredSize(new Dimension(600, 600));
             var basic = new BasicFrame();
             panel.addGLEventListener(basic);
+            panel.addMouseMotionListener(basic);
+            panel.addMouseWheelListener(basic);
 
             frame.getContentPane().add(panel);
             frame.pack();
@@ -47,11 +60,19 @@ public class Main2 {
         });
     }
 
-    static class BasicFrame implements GLEventListener, MouseMotionListener {
+    static class BasicFrame implements GLEventListener, MouseMotionListener, MouseWheelListener {
         private float rquad = 0;
         private final GLU glu = new GLU();
         private long start = System.currentTimeMillis();
         private long lastTime;
+
+        private int width;
+        private int height;
+        private int mouseX;
+        private int mouseY;
+        private float zoom = 20.0f;
+        private float pitch = 0.3f;
+        private float yaw = 0.2f;
 
         private final ArcBallCamera cam = new ArcBallCamera();
 
@@ -124,11 +145,35 @@ public class Main2 {
         });
         private ShaderState shaderState;
 
+        private int N = 50000;
+
+        private final FloatBuffer pos = genPos(N);
+        private int posVbo = -1;
+
+        private FloatBuffer genPos(int n) {
+            var rand = new Random().doubles(-10, 10).iterator();
+            var buf = GLBuffers.newDirectFloatBuffer(n * 3);
+            var pos = new Vector3f();
+            for (int i = 0; i < n; i++) {
+                pos.set(rand.nextDouble(), rand.nextDouble(), rand.nextDouble()).get(3 * i, buf);
+            }
+            return buf;
+        }
+
+        private void updatePos() {
+            var rand = new SplittableRandom().doubles(-0.01, 0.01).iterator();
+            for (int i = 0; i < pos.capacity(); i++) {
+                float x = pos.get(i);
+                x += rand.nextDouble();
+                pos.put(i, x);
+            }
+        }
+
         @Override
         public void init(GLAutoDrawable drawable) {
             final GL3 gl = drawable.getGL().getGL3();
             gl.glEnable(gl.GL_DEPTH_TEST);
-            gl.glClearColor( 0.2f, 0.3f, 0.3f, 0f );
+            gl.glClearColor( 0.1f, 0.1f, 0.1f, 0f );
 
             var vert = ShaderCode.create(gl, gl.GL_VERTEX_SHADER, 1, this.getClass(), new String[]{"vertex.vert"}, false);
             var frag = ShaderCode.create(gl, gl.GL_FRAGMENT_SHADER, 1, this.getClass(), new String[]{"fragment.frag"}, false);
@@ -157,6 +202,14 @@ public class Main2 {
             gl.glVertexAttribPointer(1, 3, gl.GL_FLOAT, false, 6 * Float.BYTES, 3 * Float.BYTES);
             gl.glEnableVertexAttribArray(1);
 
+            gl.glGenBuffers(1, vbo);
+            this.posVbo = vbo.get(0);
+            gl.glBindBuffer(gl.GL_ARRAY_BUFFER, vbo.get(0));
+            gl.glBufferData(gl.GL_ARRAY_BUFFER, pos.capacity() * Float.BYTES, pos, gl.GL_STREAM_DRAW);
+            gl.glVertexAttribPointer(2, 3, gl.GL_FLOAT, false, 3 * Float.BYTES, 0);
+            gl.glEnableVertexAttribArray(2);
+            gl.glVertexAttribDivisor(2, 1);
+
 //            gl.glGenBuffers(1, vbo);
 //            gl.glBindBuffer(gl.GL_ARRAY_BUFFER, vbo.get(0));
 //            gl.glBufferData(gl.GL_ARRAY_BUFFER, colors.capacity() * Float.BYTES, colors, gl.GL_STATIC_DRAW);
@@ -183,11 +236,15 @@ public class Main2 {
 
         @Override
         public void display(GLAutoDrawable drawable) {
-            var start = Instant.now();
+            var start = System.nanoTime();
             final GL3 gl = drawable.getGL().getGL3();
 
-            gl.glClearColor( 0.2f, 0.3f, 0.3f, 0f );
+            gl.glClearColor( 0.1f, 0.1f, 0.1f, 0f );
             gl.glClear(gl.GL_COLOR_BUFFER_BIT | gl.GL_DEPTH_BUFFER_BIT);
+
+            updatePos();
+            gl.glBindBuffer(gl.GL_ARRAY_BUFFER, this.posVbo);
+            gl.glBufferSubData(gl.GL_ARRAY_BUFFER, 0, pos.capacity() * Float.BYTES, pos);
 
             this.shaderState.useProgram(gl, true);
 
@@ -197,24 +254,32 @@ public class Main2 {
             lastTime = thisTime;
 
             var model = new Matrix4f()
+//                    .identity();
+                    .scale(0.1f)
                     .rotate(time, new Vector3f(0.5f, 1.0f, 0.0f).normalize());
-            var view = cam.viewMatrix(new Matrix4f());
+            var view = new Matrix4f()
+                    .translation(0, 0, -zoom)
+                    .rotateX(pitch)
+                    .rotateY(yaw);
             var projection = new Matrix4f()
-                    .perspective(((float) Math.toRadians(45.0)), 1.0f, 0.1f, 100.0f);
+                    .perspective(((float) Math.toRadians(30.0)), ((float) width) / height, 0.1f, 100.0f);
 
             shaderState.uniform(gl, new GLUniformData("model", 4, 4, model.get(matBuffer)));
             shaderState.uniform(gl, new GLUniformData("view", 4, 4, view.get(matBuffer)));
             shaderState.uniform(gl, new GLUniformData("projection", 4, 4, projection.get(matBuffer)));
 
-            shaderState.uniform(gl, new GLUniformData("viewPos", 3, GLBuffers.newDirectFloatBuffer(new float[]{0.0f, 0.0f, 3.0f})));
+            var viewPos = view.transformPosition(new Vector3f()).negate();
+            shaderState.uniform(gl, new GLUniformData("viewPos", 3, viewPos.get(GLBuffers.newDirectFloatBuffer(3))));
 
             shaderState.uniform(gl, new GLUniformData("material.ambient", 3, GLBuffers.newDirectFloatBuffer(new float[]{1.0f, 0.5f, 0.31f})));
             shaderState.uniform(gl, new GLUniformData("material.diffuse", 3, GLBuffers.newDirectFloatBuffer(new float[]{1.0f, 0.5f, 0.31f})));
             shaderState.uniform(gl, new GLUniformData("material.specular", 3, GLBuffers.newDirectFloatBuffer(new float[]{0.5f, 0.5f, 0.5f})));
             shaderState.uniform(gl, new GLUniformData("material.shininess", 32.0f));
 
-            shaderState.uniform(gl, new GLUniformData("dirLight.direction", 3, GLBuffers.newDirectFloatBuffer(new float[]{-0.2f, -1.0f, -0.3f})));
-            shaderState.uniform(gl, new GLUniformData("dirLight.ambient", 3, GLBuffers.newDirectFloatBuffer(new float[]{0.1f, 0.1f, 0.1f})));
+//            var lightDir = new Vector3f(-1, -1, -1).rotateX(time);
+            var lightDir = new Vector3f(1, 1, 1);
+            shaderState.uniform(gl, new GLUniformData("dirLight.direction", 3, lightDir.get(GLBuffers.newDirectFloatBuffer(3))));
+            shaderState.uniform(gl, new GLUniformData("dirLight.ambient", 3, GLBuffers.newDirectFloatBuffer(new float[]{0.2f, 0.2f, 0.2f})));
             shaderState.uniform(gl, new GLUniformData("dirLight.diffuse", 3, GLBuffers.newDirectFloatBuffer(new float[]{0.6f, 0.6f, 0.6f})));
             shaderState.uniform(gl, new GLUniformData("dirLight.specular", 3, GLBuffers.newDirectFloatBuffer(new float[]{0.6f, 0.6f, 0.6f})));
 
@@ -222,30 +287,48 @@ public class Main2 {
 //            int vertexColorLoc = gl.glGetUniformLocation(this.shader.program(), "ourColor");
 //            gl.glUniform4f(vertexColorLoc, 0.0f, greenValue, 0.0f, 1.0f);
 //            gl.glDrawElements(gl.GL_TRIANGLES, indices.capacity(), gl.GL_UNSIGNED_INT, 0);
-            gl.glDrawArrays(gl.GL_TRIANGLES, 0, vertices.capacity());
 
-            System.out.println(Duration.between(start, Instant.now()).toNanos() / 1_000_000.0);
+//            gl.glDrawArrays(gl.GL_TRIANGLES, 0, vertices.capacity());
+
+            gl.glDrawArraysInstanced(gl.GL_TRIANGLES, 0, vertices.capacity(), N);
+
+            System.out.println((System.nanoTime() - start) / 1.0e6);
         }
 
         @Override
         public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
             final GL3 gl = drawable.getGL().getGL3();
-            if( height <= 0 )
-                height = 1;
 
-            final float h = ( float ) width / ( float ) height;
-            gl.glViewport( 0, 0, width, height );
-
-//            glu.gluPerspective( 45.0f, h, 1.0, 20.0 );
+            // wtf
+            var scale = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice().getDefaultConfiguration().getDefaultTransform().getScaleX();
+            width = width * (int)scale;
+            height = height * (int)scale;
+            this.width = width;
+            this.height = height;
+            gl.glViewport( 0, 0, width, height);
         }
 
         @Override
         public void mouseDragged(MouseEvent e) {
+            yaw += Math.toRadians((e.getX() - mouseX) * 1.01f);
+            pitch += Math.toRadians((e.getY() - mouseY) * 1.01f);
+            mouseX = e.getX();
+            mouseY = e.getY();
         }
 
         @Override
         public void mouseMoved(MouseEvent e) {
+            this.mouseX = e.getX();
+            this.mouseY = e.getY();
+        }
 
+        @Override
+        public void mouseWheelMoved(MouseWheelEvent e) {
+            if (e.getPreciseWheelRotation() < 0) {
+                zoom /= 1.05f;
+            } else {
+                zoom *= 1.05f;
+            }
         }
     }
 }
--- src/main/resources/vertex.vert
+++ src/main/resources/vertex.vert
@@ -1,6 +1,7 @@
 #version 330 core
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aNormal;
+layout (location = 2) in vec3 aInstancePos;
 
 out vec3 Normal;
 out vec3 FragPos;
@@ -10,7 +11,9 @@ uniform mat4 view;
 uniform mat4 projection;
 
 void main() {
-    gl_Position = projection * view * model * vec4(aPos, 1.0);
+    vec4 worldPos = model * vec4(aPos, 1.0);
+
+    gl_Position = projection * view * (worldPos + vec4(aInstancePos, 0.0));
     FragPos = vec3(model * vec4(aPos, 1.0));
     Normal = mat3(transpose(inverse(model))) * aNormal;
 }
